bd7a9e76654bb72458c781d13bcaf41b436a58ba
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-07-07 23:42:48 +0300

    question 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""concatenating strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns a string"""
+    my_string = str1 + str2
+    return my_string
