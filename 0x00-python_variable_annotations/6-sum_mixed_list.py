53dacc9a0e0f5f2fb4b883e29e862ca4d10ace1d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-07-08 08:57:52 +0300

    question 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Sum of mixed list"""
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List) -> float:
+    """
+    function sum_mixed_list which takes a list
+    mxd_lst of integers and floats and returns their sum as a float.
+    """
+    sum = 0
+    for n in mxd_lst:
+        sum += n
+    return sum
