44e55b465485020eaf6a9fbe195aee5a196c0f53
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-07-14 15:56:44 +0300

    question 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,19 +8,25 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Async routine that spawns wait_random n times with the specified
-    max_delay.
-
-    Args:
-        n (int): The number of times to wait.
-        max_delay (int): The maximum delay to wait for, in seconds.
-
-    Returns:
-        List[float]: list of all the delays in ascending order.
+async def wait_n(n: int, max_delay: int) -> float:
     """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    async routine called wait_n that takes in 2
+    int arguments (in this order): n and max_delay
+    """
+    my_list = []
+    for i in range(n):
+        my_list.append(wait_random(max_delay))
+    
+    delays = await asyncio.gather(*tasks)
+
+    sorted_delays = []
+
+    for delay in delays:
+        for i in range(len(sorted_delays)):
+            if delay < sorted_delays[i]:
+                sorted_delays.insert(i, delay)
+                break
+        else:
+            sorted_delays.append(delay)
+    
+    return sorted_delays
